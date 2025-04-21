58f94473b2bad664b68b5cc9fe36116bee6de43e
alesia <alesya5704747@icloud.com>
2025-04-21 21:17:12 +0300

    init: project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route,Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route,Navigate, HashRouter } from 'react-router-dom';
 import { Card } from './components/card/Card'
 import { CreateCard } from './components/create-card/CreateCard'
 import { CardList } from './components/list-of-cards/CardList'
@@ -7,13 +7,13 @@ import {InfoCard} from './components/infoCard/InfoCard'
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route  path='/'  element={<CardList />}></Route>
       <Route path="/create-card" element={<CreateCard></CreateCard>}></Route>
       <Route path='/:id' element ={<InfoCard/>}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
